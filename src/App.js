42f5c4d79039392dbe0b94588edf0ec503d4201e
Programmer-learner123 <learnerakansha@gmail.com>
2020-06-25 18:05:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Button, Form, FormGroup, Label, Input} from 'reactstrap';
+import { FacebookLoginButton, GoogleLoginButton } from 'react-social-login-buttons';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+    return(
+    
+      <Form className="login-form border border-success bg-light">
+      <h1><span className="font-weight-light">Sign In!</span></h1>
+      
+      <FormGroup>
+      <div className="container">
+      <div className="row">
+      <div class="col-12-md-6-md-6">
+      <FacebookLoginButton />
+      </div>
+      <div class="col-12-md-6-md-6">
+      <GoogleLoginButton />
+      </div>
+      </div>
+      </div>
+      </FormGroup>
+      
+      <FormGroup className="text-center"><span className="font-weight-light">or</span></FormGroup>
+      <FormGroup>
+        <Input type="email" placeholder="Email"/>
+      </FormGroup>
+
+      <FormGroup>
+      <Input type="password" placeholder="Password"/>
+      </FormGroup>
+      <Button className="btn-lg btn-block btn-success">LOG IN</Button>
+      <FormGroup>
+      <a href="#"className="nounderline text-danger"><div className="text-right font-weight-light "> Forgot Your Password?</div>
+      </a>
+      </FormGroup>
+      <FormGroup>
+      <span className="text-left font-weight-light">Don't Have An Account Yet?</span>
+      <Button className="btn btn-light btn-outline-dark float-right">SIGN UP!</Button>
+      </FormGroup>
+      
+      </Form>
+    
+    );
+  }
+
 }
-
 export default App;
+
+
+
