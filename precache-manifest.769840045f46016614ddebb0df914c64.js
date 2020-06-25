6c29d352f0167fb65d81825b07e7404d0059fad4
Programmer-learner123 <learnerakansha@gmail.com>
2020-06-25 18:06:31 +0530

    Updates

---
+++ precache-manifest.769840045f46016614ddebb0df914c64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28d272d48477445c17de89c202e1981a",
+    "url": "/ReactstrapSignIn/index.html"
+  },
+  {
+    "revision": "948732df4a8ebfe8e536",
+    "url": "/ReactstrapSignIn/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "ea01d79e905de24bac48",
+    "url": "/ReactstrapSignIn/static/css/main.eca5090e.chunk.css"
+  },
+  {
+    "revision": "948732df4a8ebfe8e536",
+    "url": "/ReactstrapSignIn/static/js/2.13039c0f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ReactstrapSignIn/static/js/2.13039c0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea01d79e905de24bac48",
+    "url": "/ReactstrapSignIn/static/js/main.3c546574.chunk.js"
+  },
+  {
+    "revision": "0abc93d41771f980d7d5",
+    "url": "/ReactstrapSignIn/static/js/runtime-main.bc66adc6.js"
+  }
+]);
